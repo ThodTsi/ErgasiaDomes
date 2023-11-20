39dafbddc9052429e11f4889390389facb6a3819
ThodTsi <teotsi4002@gmail.com>
2023-11-20 16:32:26 +0200

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,32 @@
+public class Node {
+    private String data;
+    protected Node next;
+    protected Node prev;
+
+    Node(String data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public void setNext(Node n) {
+        this.next = n;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+
+    public void setPrev(Node p) {
+        this.next = p;
+    }
+
+    public Node getPrev() {
+        return this.prev;
+    }
+
+}
