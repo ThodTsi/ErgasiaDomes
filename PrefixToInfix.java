0708a956ce7de35a31e96a9581a7efb1953dfaaf
ThodTsi <teotsi4002@gmail.com>
2023-11-24 18:34:24 +0200

    Update PrefixToInfix.java

--- PrefixToInfix.java
+++ PrefixToInfix.java
@@ -8,40 +8,37 @@ public class PrefixToInfix {
         String c = in.nextLine();
         char[] prefix = c.toCharArray();
         StringDoubleEndedQueueImpl queue = new StringDoubleEndedQueueImpl();
-        for (int i = 0; i < prefix.length; i++) {
-            queue.addLast(Character.toString(prefix[i]));
-        }
         boolean pref = true;
-        // for (int i = 0; i < prefix.length; i++) {
-        // if (!((prefix[i] >= '0' & prefix[i] <= '9') || (prefix[i] == '+') ||
-        // (prefix[i] == '-')
-        // || (prefix[i] == '*') || (prefix[i] == '/'))) {
-        // pref = false;
-        // }
-        // }
-        Node node = queue.head;
-        while (n != null) {
-            String d = node.getData();
-            if (!((d.charAt(0) >= '0' & d.charAt(0) <= '9') || (d.equals("+")) || (d.equals("-")) || (d.equals("*"))
-                    || (d.equals("/")))) {
+        int telestes = 0;
+        int nums = 0;
+        for (int i = 0; i < prefix.length; i++) {
+            if (!((prefix[i] >= '0' & prefix[i] <= '9') || (prefix[i] == '+') ||
+                    (prefix[i] == '-')
+                    || (prefix[i] == '*') || (prefix[i] == '/'))) {
                 pref = false;
+            } else if ((prefix[i] == '-') || (prefix[i] == '+') || (prefix[i] == '*') || (prefix[i] == '/')) {
+                telestes += 1;
+            } else if (prefix[i] >= 0 & prefix[i] <= '9') {
+                nums += 1;
             }
-            node = node.next;
         }
+        if (telestes > nums) {
+            pref = false;
+        }
+        int pos = 1;
+
         if (pref) {
+
             int count = 0;
             boolean flag = false;
-            Node t = queue.tail;
-            Node h = queue.head;
-            while (t != null) {
-                if ((t.getData().equals("+")) || (t.getData().equals("-")) || (t.getData().equals("*"))
-                        || (t.getData().equals("/"))) {
-                    if (flag == false) {
-                        if (t.charAt(0) >= '0' & t.charAt(0) <= '9') {
-                            String temp = queue.removeFirst();
-                            queue.addLast(temp);
-                            h = h.next;
-                            while (flag == false & count < prefix.length) {
+            for (int i = 0; i < prefix.length; i++) {
+                if ((prefix[i] == '+') || (prefix[i] == '-') || (prefix[i] == '*') ||
+                        (prefix[i] == '/')) {
+                    if (count < prefix.length) {
+                        if (prefix[count] >= '0' & prefix[count] <= '9') {
+                            queue.addLast(Character.toString(prefix[count]));
+                            flag = false;
+                            while (flag == false) {
                                 if (prefix[count] >= '0' & prefix[count] <= '9') {
                                     queue.addLast(Character.toString(prefix[i]));
                                     queue.addLast(Character.toString(prefix[count]));
@@ -49,27 +46,28 @@ public class PrefixToInfix {
                                 }
                                 count++;
                             }
-
-                        }
-                    } else {
-                        boolean stop = false;
-                        while (stop == false & count < prefix.length) {
-                            if (prefix[count] >= '0' & prefix[count] <= '9') {
-                                queue.addLast(Character.toString(prefix[i]));
-                                queue.addLast(Character.toString(prefix[count]));
-                                stop = true;
-                            }
-                            count++;
                         }
                     }
+                    /*
+                     * else {
+                     * boolean stop = false;
+                     * while (stop == false & count < prefix.length) {
+                     * if (prefix[count] >= '0' & prefix[count] <= '9') {
+                     * queue.addLast(Character.toString(prefix[i]));
+                     * queue.addLast(Character.toString(prefix[count]));
+                     * stop = true;
+                     * }
+                     * count++;
+                     * }
+                     * }
+                     */
                 }
 
+                queue.printQueue(System.out);
+
+                in.close();
+
             }
-            queue.printQueue(System.out);
         }
-
-        in.close();
-
     }
-
 }
